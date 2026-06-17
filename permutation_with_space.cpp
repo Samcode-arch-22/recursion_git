b22c336c97eb278f8da3f5494780b5d3a13fb3fd
SOUMYA PRAKASH <soumyaprakashbhosagar23@gmail.com>
2026-06-17 12:31:34 +0530

    permutation_with_space
    the first step will be ( the step where only i had i choice )
    
    1bit doubt on :- //string op = "";
    if(!ip.empty()){
        op.push_back(ip[0]);
        ip.erase(0,1);
    part

---
+++ permutation_with_space.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+void solve(string ip, string op){
+
+    if(ip.length()==0){
+        cout<<op<<endl;
+        return;
+    }
+ 
+    string op1=op;
+    string op2=op;
+
+    op1.push_back(' ');
+    op1.push_back(ip[0]);
+
+    op2.push_back(ip[0]);
+    ip.erase(0,1);
+
+    solve(ip,op1);
+    solve(ip,op2);
+
+    return;
+}
+int main(){
+
+    string ip;
+    cout<<"enter the string: ";
+    cin>>ip;
+
+
+string op = "";
+if(!ip.empty()){
+    op.push_back(ip[0]);
+    ip.erase(0,1);
+}
+
+    solve(ip,op);
+
+    return 0;
+
+}
