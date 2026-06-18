0133f1a5d06aa981c74de698dacdb8f1db198ed7
SOUMYA PRAKASH <soumyaprakashbhosagar23@gmail.com>
2026-06-18 10:39:38 +0530

    adityverma_recursion

---
+++ print_n_bit_binary_number_havemre_1_than_0.cpp.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+
+void solve(int one,int zero, int n, string op){
+
+    if(n==0){
+        cout<<op<<endl;
+        return;
+    }
+
+    string op1=op;
+    op1.push_back('1');
+    solve(one+1,zero,n-1,op1);
+
+    if(one>zero){
+        string op2=op;
+        op2.push_back('0');
+        solve(one,zero+1,n-1,op2);
+    }
+
+    return;
+}
+int main(){
+
+    int n;
+    cout<<"Enter the number of bits: ";
+    cin>>n;
+
+    int one =0;
+    int zero=0;
+
+    string op="";
+
+    solve(one , zero, n, op);
+
+    return 0;
+}
